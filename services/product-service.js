d539ea75e3f88f322ea7ed118138685dc0f2ed61
Lyzen Vira <133158996+LyzenVira@users.noreply.github.com>
2024-01-12 09:08:53 +0200

    fix: update response type data Product

--- services/product-service.js
+++ services/product-service.js
@@ -15,10 +15,12 @@ class ProductService {
 
   async getProductWithMetafields(productId) {
     try {
-      const product = await shopifyService.shopify.product.get(productId);
-
+      const product = await shopifyService.shopify.product.get(productId, {
+			fields: "id,title,body_html,product_type,created_at,updated_at,handle,variants, image, images, options"
+		 });
       const metafields = await shopifyService.shopify.metafield.list({
         metafield: { owner_resource: "product", owner_id: productId },
+		   fields: "id, namespace, value"
       });
 
       const productWithMetafields = {
